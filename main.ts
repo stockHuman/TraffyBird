200a04072ea69e4b5abbc48590e4d95837138e80
L. H. N <82917144+l198881@users.noreply.github.com>
2021-08-26 19:00:09 -0300

    Update and rename main.js to main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+ {app, BrowserWindow} = require('electron')
+ path = require('path')
 
-function createWindow () {
+ createWindow () 
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  const mainWindow = new BrowserWindow(
     width: 300,
     height: 520,
-  })
+  )
 
   mainWindow.setContentSize(288, 480)
   mainWindow.setMenuBarVisibility(false)
@@ -30,13 +30,13 @@ app.allowRendererProcessReuse = true
 app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
+    (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
 
 // In this file you can include the rest of your app's specific main process
